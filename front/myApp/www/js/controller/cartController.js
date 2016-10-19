145e700440b5c27797f0514eb9f4d12c04c1777b
vanbleusa <a.vanbleus@gmail.com>
2016-10-19 21:54:48 +0200

    Renommage images + form candidature

--- front/myApp/www/js/controller/cartController.js
+++ front/myApp/www/js/controller/cartController.js
@@ -1,10 +1,34 @@
 angular.module('app.controllers')
-.controller('CartController', function($scope, $rootScope, $state) {
+.controller('CartController', function($scope, $rootScope, $ionicModal, $filter) {
     // Get current user offers in cart
     $scope.offers = $rootScope.cartOffers;
     
-    // Go to apply view
+    // Apply form viewModel
+    $scope.applyFormViewModel = {};
+    
+    // Create the apply modal
+    $ionicModal.fromTemplateUrl('templates/applyForm.html', {
+        scope: $scope
+    }).then(function(modal) {
+        $scope.applyForm = modal;
+    });
+    
+    // Open apply form
     $scope.apply = function() {
-        $state.go('app.cart');
+        $scope.applyForm.show();
+    };
+    
+    // Launch user appliance to current 
+    $scope.doApply = function() {
+        console.log("Apply form : ", $scope.applyFormViewModel);
+        $scope.applyForm.hide();
+    };
+    
+    // Remove offer from user's cart
+    $scope.removeOffer = function(id) {
+        $rootScope.cartOffers = $filter("filter")($rootScope.cartOffers, { id:id });
+        var offerIndex = $rootScope.cartOffers.indexOf(offerToRemove);
+        $rootScope.cartOffers.splice(offerIndex, 1);  
+        console.log(offerToRemove);  
     };
 });
--- front/myApp/www/js/controller/internOfferController.js
+++ front/myApp/www/js/controller/internOfferController.js
@@ -6,13 +6,29 @@ angular.module('app.controllers')
     
     // Déclaration de l'offre
     $scope.offer = { 
-        label: 'CGI - Poste d\'alternance pour gens souhaitant rester pauvre',
-        id: $scope.offerId,
-        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse ut faucibus lectus. Ut enim purus, ullamcorper at ligula nec, mattis accumsan ex. Nullam eu iaculis augue, egestas vulputate ante. Aliquam vel eleifend magna, a facilisis turpis. Donec nec nisl convallis, cursus lorem sed, rhoncus quam. Etiam quis enim arcu. Nunc non pretium sem. Sed sit amet bibendum ex. Maecenas ac lacus ac sem molestie viverra. Mauris pretium elit nunc, non volutpat ex tristique vitae. Sed quis dui dui. Suspendisse potenti. Suspendisse pellentesque lacus vitae elit ultricies, nec ullamcorper odio pellentesque. Nunc efficitur, mauris et dignissim. retium sem. Sed sit amet bibendum ex. Maecenas ac lacus ac sem molestie viverra. Mauris pretium elit nunc, non volutpat ex tristique vitae. Sed quis dui dui. Suspendisse potenti. Suspendisse pellentesque lacus vitae elit ultricies, nec ullamcorper odio pellentesque. Nunc efficitur, mauris et dignissim.',
-        earning: '1000000€',
-        referent: 'JEAN BOMBER',
-        location: 'PAR ICI ET PAR LA',
-        duration: 'Jusqu\'à ce que ton âme soit consumé'
+        id: 1, 
+        label: 'Développeur',
+        earning: '10K€',
+        description: 'description test 1',
+        location: 'Nulle part',
+        duration: '1 jour',
+        referent: {
+            name: 'Jean BOMBER',
+            company : {
+                name: 'CGI',
+                logoLink: 'http://3.bp.blogspot.com/-6AOA3ACYmos/UPZGQCqEwYI/AAAAAAAAVnM/91uDFflFQEs/s1600/CGI+logo+2013.png'
+            }
+        },
+        currentApply: {
+            state: 1,
+            stateLabel: 'disponible'
+        },
+        applies: [
+            {
+                state: 1,
+                stateLabel: 'disponible'
+            }
+        ]
     };
     
     // Add current offer to user's cart
