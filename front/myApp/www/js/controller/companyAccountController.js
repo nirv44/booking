dfcc5df4f63b9727194ec8733939a8d8a7010d8a
vanbleusa <a.vanbleus@gmail.com>
2016-10-20 14:00:49 +0200

    COR companyAccountController

--- front/myApp/www/js/controller/companyAccountController.js
+++ front/myApp/www/js/controller/companyAccountController.js
@@ -1,10 +1,5 @@
 angular.module('app.controllers')
 .controller('CompanyAccountController', function($scope, $stateParams) {
-    
-    if (!$stateParams.companyId) throw new Error("No id passed to CompanyController");
-    // Récupération de l'id de l'offre en cours
-    $scope.companyId = $stateParams.companyId;
-    
     // Mock
     $scope.company = { 
         id: 1, 
