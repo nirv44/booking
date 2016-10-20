373c7464957cc52d5e2819733856a7553b00ecce
vanbleusa <a.vanbleus@gmail.com>
2016-10-20 11:51:37 +0200

    DEL appel interOffers

--- front/myApp/www/js/controller/internOffersController.js
+++ front/myApp/www/js/controller/internOffersController.js
@@ -3,37 +3,6 @@ angular.module('app.controllers')
     
     // ViewModel for filterModal
     $scope.filterViewModel = {};
-
-    $scope.ICIJERECUPEREMESSUPPERTINTERNOFFERMAISILFAUTCHANGGERLENOMDELAFOCNTION = function(){
-        var req = {
-            method: 'GET',
-            URL: $rootScope.serverURL+'/internoffer'
-        }
-        $http(req).then(
-            function(response){
-                // ICI AVEC response.data y a toutes les données
-                //response.data;
-
-                // ET LA IL FAUT RECUPE LA COMPANY LI2E A LIDEE RECUP
-                var reqCompany = {
-                    method: 'GET',
-                    URL: $rootScope.serverURL+'/company/'+response.data.referent
-                }
-                $http(reqCompany).then(
-                    function(responseCompany){
-                        // Oh la j'ai la company par 
-                        //responseCompany.data;
-                    },function(responseCompany){
-
-                    });
-                // PAREIL POUR CURRENTAPPLY
-
-                // ICI PAREIL POUR LE APPLY
-            }, function(response){
-
-            });
-        );
-    }
         
     // Mock offers
     var mockOffers = [
